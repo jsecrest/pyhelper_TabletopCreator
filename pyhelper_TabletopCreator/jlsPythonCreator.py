b9ed163b4a76a11628183dcf51871c6b6539d2d4
Jason Secrest <jlsecrest@gmail.com>
2023-03-02 12:05:20 +0000

    moved csv prep outside of with open statement

--- pyhelper_TabletopCreator/jlsPythonCreator.py
+++ pyhelper_TabletopCreator/jlsPythonCreator.py
@@ -90,23 +90,26 @@ for _, set_dict in sets_json_load.items():
     print(f"Set name: {set_dict['name']}")
     if set_dict["name"] == set_to_modify:
         print("\t- Set name matches!")
-        # print(set_dict.keys()) #for testing
+
+        # extract data from json and restructure it
         my_set["name"] = set_dict["name"]
         my_set["fields_and_samples"] = []
         my_set["fields_and_samples"] = get_csv_fields_and_samples(set_dict["items"])
+
+        # prep to write csv
         csv_filename = f"pyhelper_TabletopCreator/{my_set['name']} - set_from_json_{get_current_timestamp()}.csv"
         csv_columns = list(my_set["fields_and_samples"][0].keys())
         csv_samplerow = list(my_set["fields_and_samples"][0].values())
+        # make title the first column
+        title_index = csv_columns.index("title")
+        csv_columns.insert(0, csv_columns.pop(title_index))
+        # then make setname the first column
+        csv_columns.insert(0, "setname")
+        # make the sample row output match the column changes above
+        csv_samplerow.insert(0, csv_samplerow.pop(title_index))
+        csv_samplerow.insert(0, my_set["name"])
 
         with open(csv_filename, "w") as csv_file:
             csv_writer = csv.writer(csv_file)
-            # make title the first column
-            title_index = csv_columns.index("title")
-            csv_columns.insert(0, csv_columns.pop(title_index))
-            # then make setname the first column
-            csv_columns.insert(0, "setname")
-            # make the sample row output match the column changes above
-            csv_samplerow.insert(0, csv_samplerow.pop(title_index))
-            csv_samplerow.insert(0, my_set["name"])
             csv_writer.writerows([csv_columns, csv_samplerow])
         print(f"File written: {csv_filename}")
