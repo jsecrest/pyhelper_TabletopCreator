eaf4bef728c15a9f61d3b09a62e10bc82c5006c5
Jason Secrest <jlsecrest@gmail.com>
2023-03-02 01:14:35 -0700

    incomplete changes
    swapping to other computer

--- pyhelper_TabletopCreator/jlsPythonCreator.py
+++ pyhelper_TabletopCreator/jlsPythonCreator.py
@@ -3,15 +3,22 @@ import glob
 import pprint
 import json
 
+blueprint_set_files = glob.glob("data/blueprints/*.json")
 component_set_files = glob.glob("data/sets/*.json")
-print(component_set_files)
+# print(component_set_files)
 
-component_set_dict = dict()
+bluepirnts = dict()
+sets = dict()
+
+for filename in blueprint_set_files:
+    with open(filename) as json_file:
+        master_dict['blueprints'][filename] = json.load(json_file)
 
 for filename in component_set_files:
     with open(filename) as json_file:
         component_set_dict[filename] = json.load(json_file)
     # pprint.pprint(component_set_dict[sf])
+
 
 for set_name, set_dict in component_set_dict.items():
     # print(type(set_dict))
