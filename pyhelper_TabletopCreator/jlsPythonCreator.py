0c30dbaf907fed2d083b8dce3c0135273c455783
Jason Secrest <jlsecrest@gmail.com>
2023-03-02 11:24:55 +0000

    Correctly remove test print statement

--- pyhelper_TabletopCreator/jlsPythonCreator.py
+++ pyhelper_TabletopCreator/jlsPythonCreator.py
@@ -93,4 +93,3 @@ for _, set_dict in sets_json_load.items():
         my_set["name"] = set_dict["name"]
         my_set["fields_and_samples"] = []
         my_set["fields_and_samples"] = get_csv_fields_and_samples(set_dict["items"])
-        pprint.pprint(my_set)
